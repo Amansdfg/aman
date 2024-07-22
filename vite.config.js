90e2550d1405b724a44571701c6bfd7c41db7a4d
AmanKalabay <149198402+Amansdfg@users.noreply.github.com>
2024-07-22 16:53:57 +0500

    asd

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,11 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
 
-// export default defineConfig({
-//   base: '/aman/',
-//   plugins: [react()],
-//   build: {
-//     outDir: 'dist'
-//   }
-// });
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/aman/', // Replace <repository-name> with your actual GitHub repository name
+  base: '/aman/', 
   plugins: [react()],
-  // build: {
-  //   outDir: 'dist'
-  // }
+  build: {
+    outDir: 'dist'
+  }
 });
