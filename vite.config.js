c4e6a2c7750f5e14c4da39eeb956502821c6f60d
AmanKalabay <149198402+Amansdfg@users.noreply.github.com>
2024-07-22 23:23:53 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ export default defineConfig({
   base: '/aman/',
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        format: 'es', // Ensure ES module format is used
+      },
+    },
   }
 });
