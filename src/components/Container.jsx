c77acd4fd603d7ab90427ef0ca00bb052610cd96
AmanKalabay <149198402+Amansdfg@users.noreply.github.com>
2024-08-28 13:28:10 +0500

    asd

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,23 +2,23 @@ import img from "../assets/GGG.png"
 export default function Container(){
     return(
         <section id="container">
-        <div class="item">
+        <div className="item">
             <img className="img" src={img}/>
-            <div class="swipe">
+            <div className="swipe">
                 <h3>Lorem, ipsum. </h3>
                 <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum, soluta?</span>
             </div>
         </div>
-        <div class="item">
+        <div className="item">
             <img className="img" src={img}/>
-            <div class="swipe">
+            <div className="swipe">
                 <h3>Lorem, ipsum.</h3>
                 <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum, soluta?</span>
             </div>
         </div>
-        <div class="item">
+        <div className="item">
             <img className="img" src={img}/>
-            <div class="swipe">
+            <div className="swipe">
                 <h3>Lorem, ipsum.</h3>
                 <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum, soluta?</span>
             </div>
