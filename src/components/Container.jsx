9e7190fae71e5bde11517f09050c42a2078c6309
AmanKalabay <149198402+Amansdfg@users.noreply.github.com>
2024-08-29 20:05:34 +0500

    asd

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,28 +1,45 @@
-import img from "../assets/GGG.png"
-export default function Container(){
-    return(
-        <section id="container">
-        <div className="item">
-            <img className="img" src={img}/>
-            <div className="swipe">
-                <h3>Lorem, ipsum. </h3>
-                <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum, soluta?</span>
-            </div>
+import img from "../assets/GGG.png";
+
+export default function Container() {
+  return (
+    <section id="container">
+      <div className="item">
+        <img className="img" src={img} />                  
+          <div className="swipe">
+          <h3>JSON Web Token</h3>
+            <span>
+              JSON Web Token is a proposed Internet standard for creating data
+              with optional signature and/or optional encryption whose payload
+              holds JSON that asserts some number of claims. The tokens are
+              signed either using a private secret or a public/private key.
+            </span>          
         </div>
-        <div className="item">
-            <img className="img" src={img}/>
-            <div className="swipe">
-                <h3>Lorem, ipsum.</h3>
-                <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum, soluta?</span>
-            </div>
-        </div>
-        <div className="item">
-            <img className="img" src={img}/>
-            <div className="swipe">
-                <h3>Lorem, ipsum.</h3>
-                <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum, soluta?</span>
-            </div>
-        </div>
+      </div>
+      <div className="item">
+        <img className="img" src={img} />        
+          <div className="swipe">
+          <h3>WebSocket</h3>
+            <span>
+              WebSocket is a computer communications protocol, providing a
+              simultaneous two-way communication channel over a single
+              Transmission Control Protocol (TCP) connection.
+            </span>
+          </div>        
+      </div>
+      <div className="item">
+        <img className="img" src={img} />              
+          <div className="swipe">
+          <h3>Tailwind CSS</h3>
+            <span>
+              Tailwind CSS is an open-source CSS framework. The main feature of
+              this library is that, unlike other CSS frameworks like Bootstrap,
+              it does not provide a series of predefined classes for elements
+              such as buttons or tables. Instead, it creates a list of "utility"
+              CSS classes that can be used to style each element by mixing and
+              matching.
+            </span>
+          </div>        
+      </div>
     </section>
-    )
+  );
 }
