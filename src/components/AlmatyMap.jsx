445d5e3e5b26fa1f2e7c7ec2f0eae08702193939
AmanKalabay <149198402+Amansdfg@users.noreply.github.com>
2025-01-05 21:39:04 +0500

    asd

--- src/components/AlmatyMap.jsx
+++ src/components/AlmatyMap.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker,Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
 function AlmatyMap(){
